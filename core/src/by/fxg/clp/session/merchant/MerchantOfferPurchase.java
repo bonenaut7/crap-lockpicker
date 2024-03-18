fe2fdf0888d56783d78881db9178112fecc28536
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-18 11:58:59 +0300

    rename item field, add constructor, add getters for fields

--- core/src/by/fxg/clp/session/merchant/MerchantOfferPurchase.java
+++ core/src/by/fxg/clp/session/merchant/MerchantOfferPurchase.java
@@ -7,8 +7,13 @@ import by.fxg.clp.session.items.ItemTradeable;
 
 public class MerchantOfferPurchase extends MerchantOffer {
 	private static final float PRICE_MODIFIER = 0.75f; // Price of item multiplies by this number to get purchase price
-	private ItemTradeable sellableItem;
+	private ItemTradeable itemType;
 	private int stock;
+	
+	public MerchantOfferPurchase(ItemTradeable itemType, int stock) {
+		this.itemType = itemType;
+		this.stock = stock;
+	}
 	
 	@Override
 	public MerchantSubCanvas getOfferCanvas() {
@@ -19,20 +24,28 @@ public class MerchantOfferPurchase extends MerchantOffer {
 	public boolean isOfferAvailable(GameSession session) {
 		return
 			this.stock > 0 &&									// Stock is more than 0
-			this.sellableItem.getPrice() > 0 &&					// Price is more than 0
-			session.gameInventory.hasItem(this.sellableItem); 	// Is there an item with required type in the inventory?
+			this.itemType.getPrice() > 0 &&					// Price is more than 0
+			session.gameInventory.hasItem(this.itemType); 	// Is there an item with required type in the inventory?
 	}
 	
 	@Override
 	public boolean onOfferAgreement(GameSession session) {
 		if (!this.isOfferAvailable(session)) return false;
 		
-		if (session.gameInventory.removeItem(this.sellableItem)) {
+		if (session.gameInventory.removeItem(this.itemType)) {
 			this.stock--;
-			session.gameInventory.money += this.sellableItem.getPrice() * PRICE_MODIFIER;
+			session.gameInventory.money += this.itemType.getPrice() * PRICE_MODIFIER;
 			return true;
 		}
 		
 		return false; // why it would ever happen?
+	}
+	
+	public ItemTradeable getOfferItem() {
+		return this.itemType;
+	}
+	
+	public int getStock() {
+		return this.stock;
 	}
 }
--- core/src/by/fxg/clp/session/merchant/MerchantOfferSale.java
+++ core/src/by/fxg/clp/session/merchant/MerchantOfferSale.java
@@ -6,9 +6,15 @@ import by.fxg.clp.session.items.ItemInstance;
 import by.fxg.clp.session.items.ItemTradeable;
 
 public class MerchantOfferSale extends MerchantOffer {
-	private ItemTradeable sellableItem;
-	private ItemInstance sellableItemInstance;
+	private ItemTradeable itemType;
+	private ItemInstance itemInstance;
 	private int stock;
+	
+	public MerchantOfferSale(ItemTradeable itemType, int stock) {
+		this.itemType = itemType;
+		this.itemInstance = new ItemInstance(itemType);
+		this.stock = stock;
+	}
 	
 	@Override
 	public MerchantSubCanvas getOfferCanvas() {
@@ -19,21 +25,29 @@ public class MerchantOfferSale extends MerchantOffer {
 	public boolean isOfferAvailable(GameSession session) {
 		return
 			this.stock > 0 &&												// Stock is more than 0
-			this.sellableItem.getPrice() > 0 &&								// Price is more than 0
-			session.gameInventory.money >= this.sellableItem.getPrice() &&	// Player has more money than cost of item
-			session.gameInventory.addItem(this.sellableItemInstance, true); // Simulation pass of adding item to the inventory
+			this.itemType.getPrice() > 0 &&								// Price is more than 0
+			session.gameInventory.money >= this.itemType.getPrice() &&	// Player has more money than cost of item
+			session.gameInventory.addItem(this.itemInstance, true); // Simulation pass of adding item to the inventory
 	}
 	
 	@Override
 	public boolean onOfferAgreement(GameSession session) {
 		if (!this.isOfferAvailable(session)) return false;
 		
-		if (session.gameInventory.addItem(this.sellableItemInstance.clone())) {
+		if (session.gameInventory.addItem(this.itemInstance.clone())) {
 			this.stock--;
-			session.gameInventory.money -= this.sellableItem.getPrice();
+			session.gameInventory.money -= this.itemType.getPrice();
 			return true;
 		}
 		
 		return false; // why it would ever happen?
+	}
+	
+	public ItemTradeable getOfferItem() {
+		return this.itemType;
+	}
+	
+	public int getStock() {
+		return this.stock;
 	}
 }
