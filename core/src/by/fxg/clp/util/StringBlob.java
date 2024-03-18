812410da4da75f44e5b5c286e3848cf75ba01b00
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-18 14:45:16 +0300

    Add random() to StringBlob

--- core/src/by/fxg/clp/util/StringBlob.java
+++ core/src/by/fxg/clp/util/StringBlob.java
@@ -1,5 +1,7 @@
 package by.fxg.clp.util;
 
+import com.badlogic.gdx.math.MathUtils;
+
 // it's just object that contains some string data in arrays
 // StringBlob is being read from files, each new line is each element in the array
 public class StringBlob {
@@ -11,8 +13,14 @@ public class StringBlob {
 		this.array = array;
 	}
 	
+	public String random() {
+		return this.isEmpty ? null :
+			this.array[MathUtils.random.nextInt(this.array.length)];
+	}
+	
 	public String get(int index) {
-		return this.isEmpty ? null : index > -1 && index < this.array.length ? this.array[index] : null;
+		return this.isEmpty ? null :
+			index > -1 && index < this.array.length ? this.array[index] : null;
 	}
 	
 	public int getSize() {
