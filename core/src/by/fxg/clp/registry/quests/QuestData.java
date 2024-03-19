1ac56f2e47408774af5458f2a1df28328cc9c15f
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-19 17:10:16 +0300

    Add few fields to quest data

--- core/src/by/fxg/clp/registry/quests/QuestData.java
+++ core/src/by/fxg/clp/registry/quests/QuestData.java
@@ -2,5 +2,7 @@ package by.fxg.clp.registry.quests;
 
 // order data for the level, like chests and other generation shit
 public class QuestData {
+	public boolean hasBeenAccepted = false;
+	public boolean requiredItemFound = false;
 	
 }
