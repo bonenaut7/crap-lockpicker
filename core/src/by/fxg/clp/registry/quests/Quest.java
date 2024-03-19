67b2d762911bfe5f1634fdef552b333384df99f2
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-19 08:41:14 +0300

    add quest generation, quest info fields

--- core/src/by/fxg/clp/registry/quests/Quest.java
+++ core/src/by/fxg/clp/registry/quests/Quest.java
@@ -1,52 +1,81 @@
 package by.fxg.clp.registry.quests;
 
-import com.badlogic.gdx.math.RandomXS128;
+import com.badlogic.gdx.math.MathUtils;
 
+import by.fxg.clp.registry.GameRegistry;
+import by.fxg.clp.registry.LevelLayout;
 import by.fxg.clp.registry.items.Item;
+import by.fxg.clp.registry.items.ItemTradeable;
+import by.fxg.clp.util.IntRange;
 
+/** Basic structure for quests, pretty straightforward thing except
+ *    for the questData which has serializable data in it like
+ *    containers placement, locks state and etc **/
 public final class Quest {
-	private boolean isSpecial = false; // Flag that means literally uncancellable permanent tutorial order
+	public static final IntRange AVATARS_ID = IntRange.of(0, 19);
+	public static final String TUTORIAL_DESCRIPTION = "You can always find me here, but don't forget to bring a Lungs Booster with you.";
 	
-	public final long questSeed0, questSeed1; // seeds that are required to generate that quest
+	private boolean isTutorial = false; // Flag that means literally uncancellable permanent tutorial order
+	
 	private String ownerName;
-	private int ownerIcon;
+	private int ownerIconID;
+	private int descriptionID;
+	private int difficulty;
 	private Item requiredItem; // only one :(
 	private int revenue; // (50 + rng(50)) * difficulty + item price?
+	private LevelLayout levelLayout; // level layout that will be used with this quest
 	
 	// world data for this order, like chests placement, locks data, for active levels
-	private QuestData orderData;
-	
-	// Generate tutorial order
-	public Quest(long seed0, long seed1) {
-		this.questSeed0 = seed0;
-		this.questSeed1 = seed1;
-	}
-	
-	// Generate randomly generated order
-	public Quest(RandomXS128 random) {
-		this(random, random.getState(0), random.getState(1));
-	}
-	
-	// Generate randomly generated order with specified seed
-	public Quest(RandomXS128 random, long seed0, long seed1) {
-		random.setState(seed0, seed1);
-		this.isSpecial = false;
-		this.questSeed0 = seed0;
-		this.questSeed1 = seed1;
-		
-		// FIXME complete
-	}
+	private QuestData questData;
 	
 	public static Quest createTutorialQuest() {
-		final Quest quest = new Quest(0, 0);
-		quest.isSpecial = true;
+		final Quest quest = new Quest();
+		quest.isTutorial = true;
 		quest.ownerName = "Matvey \'bonenaut7\'";
-		// FIXME set icon
+		quest.ownerIconID = -1;
+		quest.descriptionID = -1;
+		quest.difficulty = 0; // Very Easy
+		quest.requiredItem = GameRegistry.itemLungsBooster;
+		quest.revenue = 25;  // was 50 in 0.1
+		quest.levelLayout = GameRegistry.layoutTutorial;
+		return quest;
+	}
+	
+	public static Quest createRandomQuest() {
+		final Quest quest = new Quest();
+		quest.ownerName = String.format("%s %s", GameRegistry.namesFirstnames.random(), GameRegistry.namesSurnames.random());
+		quest.ownerIconID = AVATARS_ID.random();
+		quest.descriptionID = GameRegistry.dialogues.randomIndex();
+		quest.difficulty = MathUtils.random(1, 3);
+		quest.requiredItem = GameRegistry.LOOT_POOLS[quest.difficulty].random();
+		quest.revenue = 50 + (25 * quest.difficulty);
+		if (quest.requiredItem instanceof ItemTradeable) {
+			quest.revenue += ((ItemTradeable)quest.requiredItem).getPrice() * 0.5f;
+		}
 		
-		quest.revenue = 25; // was 50 in 0.1
+		while (quest.levelLayout == null) {
+			final LevelLayout layout = GameRegistry.LAYOUT_POOLS.random();
+			if (layout.difficulty.isInRange(quest.difficulty)) {
+				quest.levelLayout = layout;
+			}
+		}
+		
+		return quest;
+	}
+	
+	public void onQuestAccepted() {
+		
 	}
 	
 	public Item getRequiredItem() {
 		return this.requiredItem;
+	}
+	
+	public QuestData getOrderData() {
+		return this.questData;
+	}
+	
+	public void setOrderData(QuestData questData) {
+		this.questData = questData;
 	}
 }
