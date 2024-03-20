da4d45b48aaf34451f2a0947124e8c79034f7fac
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-20 12:44:31 +0300

    Add lock generation chance

--- core/src/by/fxg/clp/registry/LevelLayout.java
+++ core/src/by/fxg/clp/registry/LevelLayout.java
@@ -4,12 +4,14 @@ import by.fxg.clp.util.IntRange;
 
 public class LevelLayout {
 	public final String locationName;
+	public final float lockGenerationChance; // chance of lock generation
 	public final IntRange difficulty; // min-max value of difficulty which this layout will represent
 	public final IntRange containers; // min-max value of chests on the level
 	public final IntRange doorLocks; // min-max value of door locks on the level
 	
-	public LevelLayout(String locationName, IntRange difficulty, IntRange containers, IntRange doorLocks) {
+	public LevelLayout(String locationName, float lockGenerationChance, IntRange difficulty, IntRange containers, IntRange doorLocks) {
 		this.locationName = locationName;
+		this.lockGenerationChance = lockGenerationChance;
 		this.difficulty = difficulty;
 		this.containers = containers;
 		this.doorLocks = doorLocks;
