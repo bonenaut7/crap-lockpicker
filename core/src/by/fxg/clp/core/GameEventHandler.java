e507287a0339b9b061036a4993e7ad20b381cb49
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-20 09:27:13 +0300

    Complete logic for event handler

--- core/src/by/fxg/clp/core/GameEventHandler.java
+++ core/src/by/fxg/clp/core/GameEventHandler.java
@@ -1,6 +1,7 @@
 package by.fxg.clp.core;
 
 import by.fxg.clp.event.QuestEvent.QuestAcceptedEvent;
+import by.fxg.clp.registry.quests.Quest;
 import by.fxg.clp.session.GameSession;
 
 public class GameEventHandler {
@@ -20,25 +21,39 @@ public class GameEventHandler {
 	// Removes quest from the active list, charges penalty
 	public void onQuestDenied(QuestAcceptedEvent event) {
 		final GameSession session = event.getSession();
-		if (session.activeQuest == null) return; // what the hell?
+		if (event.getQuest() == null) return; // what the hell?
 		
-		session.availableQuests.removeValue(session.activeQuest, true);
-		session.activeQuest = null;
+		session.availableQuests.removeValue(event.getQuest(), true);
+		if (session.activeQuest == event.getQuest()) {
+			session.activeQuest = null;
+		}
 		
 		if (event.getQuest().hasBeenAccepted()) {
 			session.gameInventory.money -= Math.max(-100, event.getQuest().getRevenue() * 0.5f); // max debt cap
+			session.failedQuests++;
 		}
 		
+		session.refillQuests();
 		session.save();
 	}
 	
 	// Removes quest from the active list, gives money
 	public void onQuestCompleted(QuestAcceptedEvent event) {
 		final GameSession session = event.getSession();
-		if (session.activeQuest == null) return; // what the hell?
+		final Quest quest = event.getQuest();
+		if (quest == null) return; // what the hell?
 		
-		// FIXME
+		session.availableQuests.removeValue(quest, true);
+		session.activeQuest = null;
 		
+		if (session.gameInventory.removeItem(quest.getRequiredItem())) {
+			final float revenue = quest.getQuestData().requiredItemFound ? quest.getRevenue() : quest.getRevenue() * Quest.REVENUE_MODIFIER_ITEM_NOT_FOUND;
+			session.gameInventory.money += revenue;
+			session.totalEarned += revenue;
+			session.completedQuests++;
+		}
+
+		session.refillQuests();
 		session.save();
 	}
 }
