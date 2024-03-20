4d3fcc81430772e7fc9ef3899ca9228505bba841
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-20 11:32:57 +0300

    Update Lock (See description)
    remove canOpenLock()
    remove getUnableToLockpickMessage()
    add canBeUnlocked()
    add canBeUnlockedInstantly()
    add onInstantUnlock()

--- core/src/by/fxg/clp/registry/merchant/locks/Lock.java
+++ core/src/by/fxg/clp/registry/merchant/locks/Lock.java
@@ -1,5 +1,6 @@
 package by.fxg.clp.registry.merchant.locks;
 
+import by.fxg.clp.registry.GameRegistry;
 import by.fxg.clp.render.canvas.lock.LockSubCanvas;
 import by.fxg.clp.session.GameSession;
 import by.fxg.clp.util.IntRange;
@@ -9,9 +10,18 @@ public abstract class Lock {
 	
 	public abstract LockState<?> createLockState();
 	
-	public abstract boolean canOpenLock(GameSession session);
+	public boolean canBeUnlocked(GameSession session) {
+		return true;
+	}
 	
-	public abstract String getUnableToLockpickMessage();
+	public boolean canBeUnlockedInstantly(GameSession session) {
+		return session.gameInventory.hasItem(GameRegistry.toolLockpick);
+	}
+	
+	// returns success or failure for unlocking process
+	public boolean onInstantUnlock(GameSession session) {
+		return session.gameInventory.removeItem(GameRegistry.toolLockpick);
+	}
 	
 	public abstract LockSubCanvas getLockCanvas();
 	
