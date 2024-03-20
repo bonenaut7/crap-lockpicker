b1f3775d8a2945bb4d53bd55f97907421fa9bf7c
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-20 12:45:32 +0300

    Complete getRandomLock()

--- core/src/by/fxg/clp/registry/merchant/locks/Lock.java
+++ core/src/by/fxg/clp/registry/merchant/locks/Lock.java
@@ -1,5 +1,7 @@
 package by.fxg.clp.registry.merchant.locks;
 
+import com.badlogic.gdx.math.MathUtils;
+
 import by.fxg.clp.registry.GameRegistry;
 import by.fxg.clp.render.canvas.lock.LockSubCanvas;
 import by.fxg.clp.session.GameSession;
@@ -25,7 +27,15 @@ public abstract class Lock {
 	
 	public abstract LockSubCanvas getLockCanvas();
 	
-	public static Lock getRandomLock(int difficulty) {
+	public static Lock getRandomLock(int difficulty, float noLockChance) {
+		Lock lock = null;
+		while (lock == null) {
+			final Lock tempLock = GameRegistry.REGISTERED_LOCKS.random();
+			if (tempLock.getDifficultyRange().isInRange(difficulty)) {
+				lock = MathUtils.randomBoolean(noLockChance) ? GameRegistry.lockOpened : tempLock;
+			}
+		}
 		
+		return lock;
 	}
 }
