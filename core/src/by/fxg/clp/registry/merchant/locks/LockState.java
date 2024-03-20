dc0c21bd340a2e0613a2bbc356ab4e5867ad3943
bonenaut7 <36343628+bonenaut7@users.noreply.github.com>
2024-03-20 12:10:33 +0300

    Add seeds for lock key generation

--- core/src/by/fxg/clp/registry/merchant/locks/LockState.java
+++ core/src/by/fxg/clp/registry/merchant/locks/LockState.java
@@ -1,27 +1,44 @@
 package by.fxg.clp.registry.merchant.locks;
 
+import com.badlogic.gdx.math.MathUtils;
+
 public class LockState<T extends Lock> {
 	private T lockObject;
 	private boolean isLocked;
+	private long seed0, seed1;
 	
 	public LockState(T lockObject) {
 		this(lockObject, true);
 	}
 	
 	public LockState(T lockObject, boolean isLocked) {
+		this(lockObject, isLocked, MathUtils.random.nextLong(), MathUtils.random.nextLong());
+	}
+	
+	public LockState(T lockObject, boolean isLocked, long seed0, long seed1) {
 		this.lockObject = lockObject;
 		this.isLocked = isLocked;
+		this.seed0 = seed0;
+		this.seed1 = seed1;
 	}
 	
 	public void setLocked(boolean isLocked) {
 		this.isLocked = isLocked;
 	}
 	
+	public T getLock() {
+		return this.lockObject;
+	}
+	
 	public boolean isLocked() {
 		return this.isLocked;
 	}
 	
-	public T getLock() {
-		return this.lockObject;
+	public long getSeed0() {
+		return this.seed0;
+	}
+	
+	public long getSeed1() {
+		return this.seed1;
 	}
 }
